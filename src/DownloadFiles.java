4a623f7dfa5c536304149c1e300f51c76276e76b
jayadeep9153 <120329279+jayadeep9153@users.noreply.github.com>
2025-08-22 17:45:49 +0530

    Update DownloadFiles.java

--- src/DownloadFiles.java
+++ src/DownloadFiles.java
@@ -1,7 +1,7 @@
 public class DownloadFiles {
     public static void main(String[] args) {
-        DownloadReader dr = new DownloadReader("https://libgen.li/get.php?md5=a9d8a3c7d886b2ba675fd17bfabcd47c&key=AMX4B3M690VPBIIK",
-                "Head First Java.pdf");
+        DownloadReader dr = new DownloadReader("//give the link that you want to download",
+                "where you want to save");
 
         dr.downloadReader();
     }
